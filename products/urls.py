a1fac360b7add3021cdd40b6b7d722464dd4c0ae
Lucas-Beni <lucgarcbeni@gmail.com>
2025-10-01 16:50:13 -0300

    arquivo de carrinho e funcionalidades básicas inseridas

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('<int:pk>/', DetailProduto.as_view(), name='detalhe-produto'),
+    path("carrinho/", ver_carrinho, name="ver_carrinho"),
+    path("carrinho/adicionar/<int:produto_id>/", adicionar_ao_carrinho, name="adicionar_carrinho"),
+    path("carrinho/remover/<int:produto_id>/",remover_do_carrinho, name="remover_carrinho"),
+    path("carrinho/limpar/", limpar_carrinho, name="limpar_carrinho"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, DetailView
 from .models import Produto
+from .cart import Cart
 
 # Create your views here.
 class Home(ListView):
@@ -12,3 +13,26 @@ class DetailProduto(DetailView):
     model = Produto
     template_name = "products/detalhe_produto.html"
     context_object_name = "produto"
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=--=- Carrinho -=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=--=-
+
+def adicionar_ao_carrinho(request, produto_id):
+    cart = Cart(request)
+    produto = get_object_or_404(Produto, id=produto_id)
+    cart.add(produto, quantidade=1)
+    return redirect("ver_carrinho")
+
+def remover_do_carrinho(request, produto_id):
+    cart = Cart(request)
+    produto = get_object_or_404(Produto, id=produto_id)
+    cart.remove(produto)
+    return redirect("ver_carrinho")
+
+def ver_carrinho(request):
+    cart = Cart(request)
+    return render(request, "products/carrinho.html", {"cart": cart})
+
+def limpar_carrinho(request):
+    cart = Cart(request)
+    cart.clear()
+    return redirect("ver_carrinho")
